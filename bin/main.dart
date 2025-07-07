41bfe37b690d607685582e052823772c6976085a
iliaBog(Win) <iBog@users.noreply.github.com>
2025-07-07 11:45:04 +0300

    update

--- bin/main.dart
+++ bin/main.dart
@@ -7,11 +7,6 @@ void main(List<String> args) async {
     log.info('ai_tags_remover v0.7.0');
     return;
   }
-  log.onRecord.listen((record) {
-    // ignore: avoid_log.info
-    log.info('${record.level.name}: ${record.message}');
-  });
-
   final directory = args.isNotEmpty ? args[0] : '.';
   await processDirectory(Directory(directory));
 }
